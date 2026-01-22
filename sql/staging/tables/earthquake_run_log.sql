1e4708c55981e683430e32de8b7e8d3d5f7ae348
Bob Clinton <bob.clinton@solutiondesign.com>
2026-01-22 09:52:48 -0600

    added log tables

---
+++ sql/staging/tables/earthquake_run_log.sql
@@ -0,0 +1,25 @@
+/****** Object:  Table [dbo].[earthquake_run_log]    Script Date: 1/22/2026 9:51:32 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE IF EXISTS [dbo].[earthquake_run_log]
+
+CREATE TABLE [dbo].[earthquake_run_log](
+	[runDate] [datetime] NOT NULL,
+	[starttime] [varchar](50) NULL,
+	[endtime] [varchar](50) NULL,
+	[rows] [int] NULL,
+ CONSTRAINT [PK_earthquake_run_log] PRIMARY KEY CLUSTERED 
+(
+	[runDate] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[earthquake_run_log] ADD  CONSTRAINT [DF_earthquake_run_log_runDate]  DEFAULT (getdate()) FOR [runDate]
+GO
+
+
